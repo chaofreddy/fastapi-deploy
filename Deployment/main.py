e9e6586cc812fc8eaf52d8fe26381281136a355a
chaofreddy <33469754+chaofreddy@users.noreply.github.com>
2022-07-11 18:16:27 -0400

    Add files via upload

--- Deployment/main.py
+++ Deployment/main.py
@@ -31,4 +31,4 @@ async def predict_calories(heart_rate_mean: float, altitude_mean: float, ascend_
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=5000)
